bdb7305cd004415c418c66d2769bb05afe13f301
alvar <alvar@PORTATIL-NASA>
2025-02-21 18:00:41 +0100

    0.4

---
+++ src/fp/universidad/tipos/Centro.java
@@ -0,0 +1,65 @@
+package fp.universidad.tipos;
+
+import java.util.*;
+
+public class Centro implements Comparable<Centro>{
+
+	private String nombre, direccion;
+	private int plantas, sotanos;
+	private Set<Espacio> espacios;
+
+	public Centro(String nombre, String direccion, int plantas, int sotanos) {
+		checkCentro(plantas, sotanos);
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.plantas = plantas;
+		this.sotanos = sotanos;
+		this.espacios = new HashSet<Espacio>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public int getPlantas() {
+		return plantas;
+	}
+
+	public int getSotanos() {
+		return sotanos;
+	}
+
+	public Set<Espacio> getEspacios() {
+		return espacios;
+	}
+	
+	public void nuevoEspacio(Espacio e) {
+		if (e.getPlanta() <= (this.plantas - 1) && e.getPlanta() >= (- this.sotanos)) {
+			this.espacios.add(e);
+		} else {
+			throw new IllegalArgumentException("Plana del espacio fuera del rango permitido");
+		}
+	}
+
+	public void eliminaEspacio(Espacio e) {
+		this.espacios.remove(e);
+	}
+	
+	private void checkCentro(int plantas, int sotanos) {
+		if (plantas < 1 && sotanos < 0) {
+			throw new IllegalArgumentException("Los centros deben tener al menos una planta y cero sótanos");
+		}
+	}
+	
+	public int compareTo(Centro c) {
+		int r = nombre.compareTo(c.nombre);
+		return r;
+	}
+
+
+
+}
